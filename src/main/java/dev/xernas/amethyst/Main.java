9de354d1976135be024ba9bd84a0aa309e3a6965
Xernas <Xernas78@gmail.com>
2024-01-13 10:18:27 +0100

    [RESTART] Restarting project

---
+++ src/main/java/dev/xernas/amethyst/Main.java
@@ -0,0 +1,2 @@
+package dev.xernas.amethyst;public class Main {
+}
--- src/main/java/dev/xernas/amethyst/io/util/Direction.java
+++
@@ -1,9 +0,0 @@
-package dev.xernas.amethyst.io.util;
-
-public enum Direction {
-
-    CLIENTBOUND,
-    SERVERBOUND,
-    ALL
-
-}
